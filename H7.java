9c929b58331750cbb7227ad375d9892fc0a6260d
Harshithakulal238 <harshitha.23cs046@sode-edu.in>
2024-09-24 15:58:28 +0530

    Method overriding-Human Teacher Doctor

---
+++ H7.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author smv
+ */
+class Human{
+public void job(){
+    System.out.println("Working professional");
+}
+}
+class Teacher extends Human{
+    public void job(){
+        System.out.println("Teacher");
+    }
+}
+class Doctor extends Human{
+    public void job(){
+        System.out.println("Doctor");
+    }
+}
+public class H7 {
+    public static void main(String[] args){
+        Human amar=new Human();
+        Teacher babu=new Teacher();
+        Doctor chandra=new Doctor();
+        Human dinesh=new Doctor();
+        amar.job();
+        babu.job();
+        chandra.job();
+        dinesh.job();
+    }
+    
+}
