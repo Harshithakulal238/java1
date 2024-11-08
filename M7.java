289aa4793ec1d82856a73c3958bef3754168c47d
Harshithakulal238 <harshitha.23cs046@sode-edu.in>
2024-11-08 22:22:54 +0530

    Add files via upload

---
+++ M7.java
@@ -0,0 +1,15 @@
+
+package pk1;
+import pk2.Tools2;
+public class M7 {
+
+	public static void main(String[] args) {
+		Tools2 t1=new Tools2();
+		System.out.println(t1.reverse("Harshitha"));
+		
+
+	}
+
+}
+    
+
