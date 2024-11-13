5bc25f28ad016a812a9498e1bafa007ee8dacae6
Harshithakulal238 <harshitha.23cs046@sode-edu.in>
2024-11-13 16:34:29 +0530

    Add files via upload

---
+++ Test5.java
@@ -0,0 +1,22 @@
+
+package oopj_1;
+class Animal_2{
+    public void sound(){
+        System.out.println("Animals make sound");
+        
+    }
+}
+class Dog_1 extends Animal_2{
+    public void sound(){
+        System.out.println("Barks");
+    }
+    
+}
+public class Test5 {
+    public static void main(String[] args){
+        Animal_2 obj1=new Animal_2();
+        Animal_2 obj2=new Dog_1();
+        obj1.sound();
+        obj2.sound();
+}
+}
